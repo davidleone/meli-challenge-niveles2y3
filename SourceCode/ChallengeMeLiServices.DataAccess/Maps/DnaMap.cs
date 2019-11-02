ddbc5f36ece988df0f819a799d8e87a5a1a9d97d
David Leone <davidarielleone+github@gmail.com>
2019-11-02 19:46:35 -0300

    Instalados nugets de NHibernate y FluentNHibernate para persistencia y mapeos. Agregada entidad persistible DNA

---
+++ SourceCode/ChallengeMeLiServices.DataAccess/Maps/DnaMap.cs
@@ -0,0 +1,22 @@
+﻿using ChallengeMeLiServices.DataAccess.Models;
+using FluentNHibernate.Mapping;
+
+namespace ChallengeMeLiServices.DataAccess.Maps
+{
+    /// <summary>
+    /// The DNA database mapping.
+    /// </summary>
+    public class DnaMap : ClassMap<Dna>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DnaMap"/> class.
+        /// </summary>
+        public DnaMap()
+        {
+            Table("dna");
+            Id(x => x.Id, "dna_id");
+            Map(x => x.Chain, "chain");
+            Map(x => x.IsMutant, "is_mutant");
+        }
+    }
+}
---
+++ SourceCode/ChallengeMeLiServices.DataAccess/Models/Dna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChallengeMeLiServices.DataAccess.Models
+{
+    /// <summary>
+    /// Persistable Entity for DNA.
+    /// </summary>
+    public class Dna
+    {
+        /// <summary>
+        /// Dna's ID (primary key in database).
+        /// </summary>
+        public virtual Guid Id { get; set; }
+
+        /// <summary>
+        /// Dna chain (unique key in database).
+        /// </summary>
+        public virtual string[] Chain { get; set; }
+
+        /// <summary>
+        /// true: is Mutant | false: is Human.
+        /// </summary>
+        public virtual bool IsMutant { get; set; }
+    }
+}
